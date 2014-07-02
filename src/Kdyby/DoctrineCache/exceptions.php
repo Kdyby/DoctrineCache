12155a69759b45751a2097ccd0a84fb59f3cb7e5
Filip Procházka <filip@prochazka.su>
2014-07-02 17:10:36 +0200

    Created ReversedStorageDecorator

---
+++ src/Kdyby/DoctrineCache/exceptions.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the Kdyby (http://www.kdyby.org)
+ *
+ * Copyright (c) 2008 Filip Procházka (filip@prochazka.su)
+ *
+ * For the full copyright and license information, please view the file license.txt that was distributed with this source code.
+ */
+
+namespace Kdyby\DoctrineCache;
+
+
+
+interface Exception
+{
+
+}
+
+
+
+class NotImplementedException extends \LogicException implements Exception
+{
+
+}
