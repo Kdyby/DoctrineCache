b5473636464a7218e5e1b2dd41b0677798c383b0
David Ferbr <david@ferbr.cz>
2019-12-02 14:17:48 +0100

    make doSaveDependingOnFiles() nullable again addresses https://github.com/Kdyby/DoctrineCache/issues/23

--- src/Cache.php
+++ src/Cache.php
@@ -102,8 +102,9 @@ class Cache extends \Doctrine\Common\Cache\CacheProvider
 	 * @param string[] $files
 	 * @param int $lifeTime
 	 */
-	protected function doSaveDependingOnFiles(string $id, $data, array $files, int $lifeTime = 0): bool
+	protected function doSaveDependingOnFiles(string $id, $data, array $files, int $lifeTime = null): bool
 	{
+		$lifeTime = (int) $lifeTime;
 		$dp = [NCache::TAGS => ['doctrine'], NCache::FILES => $files];
 		if ($lifeTime > 0) {
 			$dp[NCache::EXPIRE] = time() + $lifeTime;
